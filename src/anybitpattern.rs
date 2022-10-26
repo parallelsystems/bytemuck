4235c9d2a63130900cd25fd15ab3a530c86e9eac
Andrew Gazelka <andrew.gazelka@gmail.com>
2022-10-25 19:31:28 -0700

    stash

--- src/anybitpattern.rs
+++ src/anybitpattern.rs
@@ -1,10 +1,12 @@
+#[cfg(all(target_arch = "aarch64", feature = "aarch64_simd"))]
+use core::arch::aarch64;
 use core::{
   marker::{PhantomData, PhantomPinned},
   mem::ManuallyDrop,
   num::Wrapping,
 };
 
-use crate::{Zeroable};
+use crate::Zeroable;
 
 /// Marker trait for "plain old data" types that are valid for any bit pattern.
 ///
@@ -61,53 +63,6 @@ pub unsafe trait AnyBitPattern:
 
 #[cfg(feature = "min_const_generics")]
 unsafe impl<T: AnyBitPattern, const N: usize> AnyBitPattern for [T; N] {}
-
-#[cfg(not(feature = "min_const_generics"))]
-impl_unsafe_marker_for_array!(
-  AnyBitPattern,
-  0,
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-  10,
-  11,
-  12,
-  13,
-  14,
-  15,
-  16,
-  17,
-  18,
-  19,
-  20,
-  21,
-  22,
-  23,
-  24,
-  25,
-  26,
-  27,
-  28,
-  29,
-  30,
-  31,
-  32,
-  48,
-  64,
-  96,
-  128,
-  256,
-  512,
-  1024,
-  2048,
-  4096
-);
 
 #[cfg(all(target_arch = "wasm32", feature = "wasm_simd"))]
 unsafe impl AnyBitPattern for wasm32::v128 {}
